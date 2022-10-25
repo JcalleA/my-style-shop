0f0f052e53ef7fd9e99150546a99ecab1ceda6ba
Alex Delgado <alex.delgado@ias.com.co>
2022-10-25 01:55:03 -0500

    Se realizaron cambios en la estructuracion del server separando logica de controller, bd, y routes tambien se añadieron nuevas dependencias se actualizo el package.json, se agrego www.js para que ejecute toda la aplicacion. Se implementa auth.js para bloquear rutas.

--- server/models/usuarios.js
+++ server/models/usuarios.js
@@ -3,13 +3,13 @@ const Schema = mongoose.Schema;
 
 const usuarioSchema = new Schema({
     
-    nombre:{type:String, required: true},
+    nombre: {type:String, required: true},
     apellido: String,
     correo: {type:String, required: true, unique: true}, 
     password: {type:String, required: true},
-    telefono: Number,
-    hora: Date,
-    imagenUrl: String,
+    telefono: {type: String, required: false},
+    hora: {type: Date, required: false},
+    imagenUrl: {String, required: false}
 });
 
 module.exports = mongoose.model("Usuario", usuarioSchema);
