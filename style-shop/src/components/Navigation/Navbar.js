2a73b4dc4bc1188c9b4d2f818bc14ce702e97e85
Alex Delgado <alex.delgado@ias.com.co>
2022-11-03 02:54:30 -0500

    Se agrega componente de la navegacion

---
+++ style-shop/src/components/Navigation/Navbar.js
@@ -0,0 +1,118 @@
+import React, {useState} from 'react';
+//import './components.css'
+import { Link } from 'react-router-dom';
+import "../Navigation/Nabvar.css"
+
+
+const Barranav = () => {
+    let [collapsed ] = useState(true);
+    let {isNavbarCollapsed} = useState(false)
+    return (
+
+        <div className='container-fluid'>
+            <div className='header'>
+                <nav className='navbar navbar-expand-lg navbar-ligth MainMenu navbar-responsive'>
+                    <Link to='/'>
+                        <img className="img-logo logo navbar-brand" src="https://www.seekpng.com/png/detail/816-8167410_logo-image-barbe-shop-barber-logo-barber-shop.png"
+                            alt="Imagen del logo">
+                        </img>
+                    </Link>
+                    <button onClick={collapsed = !collapsed} className="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div className='pages navbar-nav ml-3'>
+                        <div className='home'>
+                            <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+                                <Link to='/'>
+                                    <a className='nav-link' >Home</a>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className='products'>
+                            <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+                                <Link to='/servicios'>
+                                    <a className='nav-link' >Servicios</a>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className='products'>
+                            <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+                                <Link to='#'>
+                                    <a className='nav-link' >About</a>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='search navbar-nav' >
+                        <div className='collapse navbar-collpase' id='navbarSupportedContent'>
+                            <div className='form-inline search'>
+                                <input  type='search' className='form-control' placeholder='search' aria-label='search'></input>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="end navbar-nav">
+                        <div className="login">
+                            <Link to="/login">
+                            <a class="nav-link">
+                                <p>Login</p>
+                            </a>
+                            </Link>
+                        </div>
+                        <div className="register">
+                            <Link to='/registro'>
+                                <a className="nav-link">
+                                    <p>Sign up</p>
+                                </a>
+                            </Link>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+
+       /* 
+        <Navbar bg='primary' variant='dark'>
+            <Container>
+                <Navbar.Brand >
+                <Link to='/'>
+                    <img className="img-logo" src="https://www.seekpng.com/png/detail/816-8167410_logo-image-barbe-shop-barber-logo-barber-shop.png"
+                        alt="Logo">
+                    </img>
+                </Link>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Link to='/'>
+                            <Button className='nav-button'>
+                                Home
+                            </Button>
+                        </Link>
+                        <Link to="/servicios">
+                            <Button>
+                                Servicios
+                            </Button>
+                        </Link>
+                        <Link to="/registro">
+                            <Button>
+                                Registrarse
+                            </Button>
+                        </Link>
+                        <Link to="/login">
+                            <Button>
+                                Login
+                            </Button>
+                        </Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>*/
+
+        
+        
+    );
+
+};
+export default Barranav;
