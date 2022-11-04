3928e06dab658ce4b8249151f47459334f1b8a3e
Alex Delgado <alex.delgado@ias.com.co>
2022-11-03 23:42:42 -0500

    Se soluciona renderizado del home

--- style-shop/src/routes/home.js
+++ style-shop/src/routes/home.js
@@ -1,34 +1,20 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import { Navigate, useParams } from "react-router-dom";
-import axios from "axios";
-import { useEffect, useState } from "react";
-
+import { Navigate } from "react-router-dom";
 
 const Home = ({ user }) => {
-
-    console.log(user)
     return (
         user ? (
         <div>
             <h1> Welcome {user.nombre}</h1>
-
-            <button onClick={Navigate("/logout")} > Sign Out</button>
+            <button onClick={() => Navigate("/logout")} > Sign Out</button>
         </div>
     ) : (
         <div>
             <h1>Welcome Tienes Cuesta Logueate </h1>
-            <button onClick={Navigate("/login")}>Sign In</button>
+            <button onClick={() => Navigate("/login")}>Sign In</button>
         </div>
     )
-        
     )
-
-
-
-
-
-
 };
 
 export default Home;
