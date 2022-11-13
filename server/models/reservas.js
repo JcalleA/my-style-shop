a4e79ecd9dfb041ace0b80a36f28903fd7af446e
Alex Delgado <alex.delgado@ias.com.co>
2022-11-13 16:20:10 -0500

    Se borra modelo de reserva

--- server/models/reservas.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const reservaSchema = new Schema({
-    hora: {type: Date, required: false},
-    usuario: { type: Schema.ObjectId, ref: "Usuarios" },
-});
-
-module.exports = mongoose.model("Servicio", reservaSchema );
