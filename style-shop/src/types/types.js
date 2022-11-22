6aad9b9b86a05db14939d3ce6d1599f34ad0e58b
Alex Delgado <alex.delgado@ias.com.co>
2022-11-21 22:33:59 -0500

    Se agrega carpeta types para mensaje de estados

---
+++ style-shop/src/types/types.js
@@ -0,0 +1,17 @@
+const types = {
+    //authLogin: "logear user en state",
+    authLogout: "logout user en state",
+    
+    authRefreshToken: "refresh token",
+  
+    userEditNombre: "editando nombre cliente o barbero",
+    userEdit: "editando cliente o barbero",
+    userSetBarberos: "seteando barberos",
+    userResetBarberos: "reseteando barberos",
+  
+    citasGetCitas: "get citas",
+    citasDeleteCita: "delete cita",
+    citasSelectBarbero: "select barbero previo",
+    citasReset: "resetear citas",
+}
+export default types;
