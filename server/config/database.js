186154f3426c6da3ad8a9f83978fb0cb83fe682e
JcalleA <kalle-1413@hotmail.com>
2022-11-25 11:44:42 -0500

    Coneccion BD

--- server/config/database.js
+++ server/config/database.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
 
-const host = "localhost";
-const port = "27017";
-const database = "style-shop";
+// const host = "localhost";
+// const port = "27017";
+// const database = "style-shop";
 
 exports.mongoConnect = () => {
-    const mongoConnection = `mongodb://${host}:${port}/${database}`;
+    // const mongoConnection = `mongodb://${host}:${port}/${database}`;
+    const mongoConnection = "mongodb+srv://Jhonk1413:ATPYH9qs-TqrS4X@cluster0.5cxeej3.mongodb.net/style_shop"
     mongoose.connect(mongoConnection, {
-       useNewUrlParser: true,
-       useUnifiedTopology: true 
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     });
     mongoose.Promise = global.Promise;
     const dbConnection = mongoose.connection;
